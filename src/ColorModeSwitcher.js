a566ca4e6207ea232c5abbc1934ca2269bb1f087
Amit Kumar <103894389+Amit-Rohila33@users.noreply.github.com>
2022-11-12 14:45:02 +0530

    Project completed

--- src/ColorModeSwitcher.js
+++ src/ColorModeSwitcher.js
@@ -15,6 +15,7 @@ const ColorModeSwitcher = props => {
       top={'4'}
       right={'4'}
       marginLeft="2"
+      zIndex={'overlay'}
       onClick={toggleColorMode}
       icon={<SwitchIcon />}
       {...props}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,7 +29,7 @@ const Header = () => {
         h={'10'}
         borderRadius={'full'}
         onClick={onOpen}
-        zIndex="10"
+        zIndex={"overlay"}
       >
         <BiMenuAltLeft size={'20'} />
       </Button>
